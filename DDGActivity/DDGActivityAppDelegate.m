ee8b3705c19d91234ce3dbf495e06951dc555cb0
Andrew W. Donoho <github@andrew.ddg.com>
2011-10-08 09:00:31 -0500

    Make DDGActivity compatible with iOS v3.x.

--- DDGActivity/DDGActivityAppDelegate.m
+++ DDGActivity/DDGActivityAppDelegate.m
@@ -56,9 +56,10 @@
 {
     // Override point for customization after application launch.
      
-    self.window.rootViewController = self.viewController;
+    [self.window addSubview: self.viewController.view];
     [self.window makeKeyAndVisible];
-    return YES;
+
+    return NO;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
